e2fd522535873d6fc12819605ee76a783783cdd5
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2020-11-03 21:35:46 +0200

    Define reset of public API functions

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -17,9 +17,24 @@ defmodule ExBanking do
   def create_user(user) do
     User.create(user)
   end
-  
+
   @spec deposit(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | banking_error
   def deposit(user, amount, currency) do
     User.deposit(user, amount, currency)
+  end
+
+  @spec withdraw(user :: String.t, amount :: number, currency :: String.t) :: {:ok, new_balance :: number} | banking_error
+  def withdraw(user, amount, currency) do
+    User.withdraw(user, amount, currency)
+  end
+
+  @spec get_balance(user :: String.t, currency :: String.t) :: {:ok, balance :: number} | banking_error
+  def get_balance(user, currency) do
+    User.get_balance(user, currency)
+  end
+
+  @spec send(from_user :: String.t, to_user :: String.t, amount :: number, currency :: String.t) :: {:ok, from_user_balance :: number, to_user_balance :: number} | banking_error
+  def send(from_user, to_user, amount, currency) do
+    User.send(from_user, to_user, amount, currency)
   end
 end
