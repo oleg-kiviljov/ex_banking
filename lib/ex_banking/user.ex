447e49a6fe1831b785f795836611ce53cf5767e5
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2020-11-03 05:00:36 +0200

    Add basic GenServer behaviour to User

--- lib/ex_banking/user.ex
+++ lib/ex_banking/user.ex
@@ -1,4 +1,16 @@
 defmodule ExBanking.User do
+  use GenServer
+
+  alias __MODULE__
+
   @enforce_keys [:name, :wallet]
   defstruct [:name, :wallet]
+
+  def init(name) do
+    {:ok, %User{name: name, wallet: MapSet.new()}}
+  end
+
+  def create(name) when is_binary(name) do
+    GenServer.start_link(__MODULE__, name, [])
+  end
 end
