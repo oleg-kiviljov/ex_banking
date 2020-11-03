ef0463ae0807892dc3deef3dbfe42a17e9462d34
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2020-11-03 18:35:57 +0200

    Add naive implementation of deposit function

--- lib/ex_banking/user.ex
+++ lib/ex_banking/user.ex
@@ -7,7 +7,7 @@ defmodule ExBanking.User do
   defstruct [:username, :wallet]
 
   def init(username) do
-    {:ok, %User{username: username, wallet: MapSet.new()}}
+    {:ok, %User{username: username, wallet: Map.new}}
   end
 
   def create(username) when is_binary(username) do
@@ -21,17 +21,47 @@ defmodule ExBanking.User do
     {:error, :wrong_arguments}
   end
 
-  def deposit(username, amount, currency) when is_binary(username) and is_binary(currency) and is_number(amount) do
-    GenServer.call(via_tuple(username), %{action: :deposit, amount: amount, currency: currency})
+  def deposit(username, amount, currency)
+  when is_binary(username)
+  and is_binary(currency)
+  and is_number(amount)
+  and amount > 0
+  do
+    case Registry.lookup(Registry.User, username) do
+      [] -> false
+      [{_pid, _}] -> GenServer.call(via_tuple(username), %{action: :deposit, amount: amount, currency: currency})
+    end
   end
 
   def deposit(_username, _amount, _currency) do
     {:error, :wrong_arguments}
   end
 
-  defp via_tuple(username), do: {:via, Registry, {Registry.User, username}}
-
   def handle_call(%{action: :deposit, amount: amount, currency: currency}, _from, state) do
-    {:reply, state, state}
+    state |>
+      add_currency(currency) |>
+      increase_balance(amount, currency) |>
+      successful_deposit(currency)
   end
+
+  defp increase_balance(state, amount, currency) do
+    %User{wallet: wallet} = state
+    new_balance = Map.get(wallet, currency)  + amount
+    wallet = Map.put(wallet, currency, new_balance)
+    put_in(state.wallet, wallet)
+  end
+
+  def add_currency(state, currency) do
+    %User{wallet: wallet} = state
+    wallet = Map.put_new(wallet, currency, 0)
+    put_in(state.wallet, wallet)
+  end
+
+  defp successful_deposit(state, currency) do
+    %User{wallet: wallet} = state
+    new_balance = Map.get(wallet, currency)
+    {:reply, new_balance, state}
+  end
+
+  defp via_tuple(username), do: {:via, Registry, {Registry.User, username}}
 end
