ed6cc0f6f36c4f21ede47fc3516c031a65056a10
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2020-11-03 05:30:00 +0200

    Add banking_error type

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,6 +1,18 @@
 defmodule ExBanking do
   alias ExBanking.{User}
 
+  @type banking_error :: {:error,
+    :wrong_arguments                |
+    :user_already_exists            |
+    :user_does_not_exist            |
+    :not_enough_money               |
+    :sender_does_not_exist          |
+    :receiver_does_not_exist        |
+    :too_many_requests_to_user      |
+    :too_many_requests_to_sender    |
+    :too_many_requests_to_receiver
+  }
+
   def create_user(user) do
     User.create(user)
   end
